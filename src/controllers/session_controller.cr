1d1458b1306a453ace02b8a20c50c61317a2c7f9
Ciaran Broderick <broderickciaran@gmail.com>
2018-04-06 20:16:20 +0100

    Indendation issues in seccion_controller

--- src/controllers/session_controller.cr
+++ src/controllers/session_controller.cr
@@ -7,14 +7,14 @@ class SessionController < ApplicationController
   def create
     user = User.find_by(:email, params["email"].to_s)
     if user && user.authenticate(params["password"].to_s)
-        session[:user_id] = user.id
-        flash[:info] = "Successfully logged in"
-        redirect_to "/"
-      else
-        flash[:danger] = "Invalid email or password"
-        user = User.new
-        render("new.slang")
-      end
+      session[:user_id] = user.id
+      flash[:info] = "Successfully logged in"
+      redirect_to "/"
+    else
+      flash[:danger] = "Invalid email or password"
+      user = User.new
+      render("new.slang")
+    end
   end
 
   def delete
