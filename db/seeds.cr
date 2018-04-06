5f103a84f7f7b7fa5b85608ee8a9539ce6f5b52f
Ciaran Broderick <broderickciaran@gmail.com>
2018-04-06 01:44:32 +0100

    amber db seed now creates all the necessary starting objects in the db, namely Nocturne and its BQ

--- db/seeds.cr
+++ db/seeds.cr
@@ -1,6 +1,22 @@
 require "../config/application.cr"
 
-user = User.new
-user.email = "admin@example.com"
-user.password = "password"
-user.save
+# Create the Nocturne Village if it's not already done
+nocturne = Village.find_by :name, "Nocturne"
+if !nocturne
+  # If it doesn't already exist, we need to make it
+  nocturne = Village.new
+  nocturne.name = "Nocturne"
+  nocturne.save
+end
+
+# Create Nocturne's Build Queue if it doesn't exist
+bq = BuildQueue.find_by :village_id, nocturne.id
+if !bq
+  bq = BuildQueue.new
+  bq.village_id = nocturne.id
+  bq.save
+  nocturne.build_queue_id = bq.id
+  nocturne.save
+end
+
+# Create the initial stuff I have planned
