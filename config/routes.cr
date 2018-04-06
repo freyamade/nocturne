b0ed0a3616b8f6346a1d4b6cc3d3ecd38e1de4cb
Ciaran Broderick <broderickciaran@gmail.com>
2018-04-06 01:42:50 +0100

    Fixed up a bit of indentation

--- config/routes.cr
+++ config/routes.cr
@@ -1,6 +1,5 @@
 Amber::Server.configure do |app|
   pipeline :web do
-  
     # Plug is the method to use connect a pipe (middleware)
     # A plug accepts an instance of HTTP::Handler
     plug Amber::Pipe::PoweredByAmber.new
@@ -13,7 +12,7 @@ Amber::Server.configure do |app|
     plug Amber::Pipe::CSRF.new
     # Reload clients browsers (development only)
     plug Amber::Pipe::Reload.new if Amber.env.development?
-  
+
     plug Authenticate.new
   end
 
@@ -26,14 +25,14 @@ Amber::Server.configure do |app|
   end
 
   routes :web do
-      get "/profile", UserController, :show
-  get "/profile/edit", UserController, :edit
-  patch "/profile", UserController, :update
-  get "/signin", SessionController, :new
-  post "/session", SessionController, :create
-  get "/signout", SessionController, :delete
-  get "/signup", RegistrationController, :new
-  post "/registration", RegistrationController, :create
+    get "/profile", UserController, :show
+    get "/profile/edit", UserController, :edit
+    patch "/profile", UserController, :update
+    get "/signin", SessionController, :new
+    post "/session", SessionController, :create
+    get "/signout", SessionController, :delete
+    get "/signup", RegistrationController, :new
+    post "/registration", RegistrationController, :create
     get "/", HomeController, :index
   end
 
