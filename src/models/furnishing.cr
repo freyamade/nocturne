e0acfdbcc3549437426fac56b49057c69b1e7655
Ciaran Broderick <broderickciaran@gmail.com>
2018-05-04 00:52:50 +0100

    Added to_s and validation methods to furnishing model

--- src/models/furnishing.cr
+++ src/models/furnishing.cr
@@ -11,4 +11,22 @@ class Furnishing < Granite::ORM::Base
   field description : String
   field ability : String
   timestamps
+
+  def to_s
+    "#{self.name} (Level #{self.level})"
+  end
+
+  # Validation
+  validate(:name, "parameter is required.", ->(furnishing : self) {
+    (furnishing != nil && furnishing.name != "")
+  })
+  validate(:level, "parameter is required and must be above 0.", ->(furnishing : self) {
+    (furnishing != nil && furnishing.level != "" && furnishing.level.not_nil!.to_i > 0)
+  })
+  validate(:description, "parameter is required.", ->(furnishing : self) {
+    (furnishing != nil && furnishing.description != "")
+  })
+  validate(:ability, "parameter is required.", ->(furnishing : self) {
+    (furnishing != nil && furnishing.ability != "")
+  })
 end
