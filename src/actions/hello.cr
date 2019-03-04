1ee56f52a2db3f081ef29a1f17c47b2f12f7e94e
freyamade <frybrdrck@gmail.com>
2019-03-04 18:17:13 +0000

    Added first handling of parameters

--- src/actions/hello.cr
+++ src/actions/hello.cr
@@ -6,8 +6,15 @@ module Nocturne
     struct Hello
       include Onyx::REST::Action
 
+      # handle params
+      params do
+        query do
+          type who : String = "Onyx" # Set a default value for this parameter
+        end
+      end
+
       def call
-        return Views::Hello.new "Onyx"
+        return Views::Hello.new params.query.who
       end
     end
   end
